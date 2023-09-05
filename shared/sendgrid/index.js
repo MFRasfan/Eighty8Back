cfaf7dfb80b89ed844f600d65331c2d612e2446d
techsyhub <techsyhub@gmail.com>
2023-09-05 21:55:36 +0500

    add error log on sendgrid

--- shared/sendgrid/index.js
+++ shared/sendgrid/index.js
@@ -3,6 +3,7 @@ const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendEmail = async ({subject, email,text, html, code}) => {
+  try {
     const msg = {
       to: email,
       from: process.env.SENDGRID_EMAIL,
@@ -11,6 +12,10 @@ const sendEmail = async ({subject, email,text, html, code}) => {
       html:  html || `<p>Your OTP code is <strong>${code}</strong>. This code will expire in 1 hour.</p>`,
     };
     await sgMail.send(msg);
+    
+  } catch (error) {
+    console.log(error)
+  }
   };
 
   module.exports={sendEmail}
